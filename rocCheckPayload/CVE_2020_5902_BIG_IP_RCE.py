c859ac4b413e25aa90cc332d0ac64ef441c37603
rocky <939555035@qq.com>
2020-07-29 10:13:35 +0800

    检测payload回显部分

--- rocCheckPayload/CVE_2020_5902_BIG_IP_RCE.py
+++ rocCheckPayload/CVE_2020_5902_BIG_IP_RCE.py
@@ -15,7 +15,7 @@ requests.packages.urllib3.disable_warnings()
 class CVE_2020_5902_BIG_IP_RCE(object):
     def __init__(self):
         self.name = "CVE_2020_5902_BIG_IP_RCE"
-
+    #检测payload部分
     def runCheck(self, targetAddr, payload):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) \
