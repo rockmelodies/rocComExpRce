335f307589cfe7b3313767e91268d23569906476
rocky <939555035@qq.com>
2020-10-22 13:38:47 +0800

    迁移代码

--- rocCheckPayload/CVE_2017_3248_weblogic.py
+++ rocCheckPayload/CVE_2017_3248_weblogic.py
@@ -28,6 +28,7 @@ class run(object):
         """
         currentTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+
         if 'https://' in targetAddr or 'http://' in targetAddr:
             pass
         else:
