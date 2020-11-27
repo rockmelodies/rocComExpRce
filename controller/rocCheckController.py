3925ead3bbb0c14956d4b113b8a80ff38b109792
rocky <939555035@qq.com>
2020-11-27 17:48:31 +0800

    修复控制器bug

--- controller/rocCheckController.py
+++ controller/rocCheckController.py
@@ -78,7 +78,8 @@ class rocCheckController(object):
                         url = targetAddr + path
                         try:
                             header_dict = ast.literal_eval(header)
-                            res = requests.get(url, headers=header_dict, timeout=15, verify=False)
+                            res = requests.post(url, verify=False, timeout=5, headers=header_dict,
+                                                proxies=proxies)
                             if expression in res.text:
                                 status_data = '[+]{} is vulnerable! {} {}'.format(targetAddr, payload, currentTime)
                                 return {'status': 20003, 'data': status_data, 'type': 'status'}
